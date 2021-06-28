0a14d752bc1fb13e8d42fcd0c08b49c6575915aa
immutablet <atcherniakhovski@gmail.com>
2021-06-27 22:59:39 -0500

    World map with country names as data.

--- js/main.js
+++ js/main.js
@@ -5,34 +5,30 @@
 //   console.log(data[0]);
 // });
 
+var width = 900;
+var height = 500;
 
-var svg = d3.select("body").append("svg").attr("width", 1020).attr("height", 600);
-var path = d3.geoPath().projection(d3.geoMercator());
-d3.json("data/world.json").then(function(world) {
-  console.log(world)
+var svg = d3.select("body").append("svg")
+var projection = d3.geoMercator();
+var path = d3.geoPath().projection(projection);
+var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
+
+d3.json(url).then(function(geojson) {
+  // svg.append("path").attr("d", path(geojson));
   svg.selectAll("path")
-    .data(topojson.feature(world,world.objects.countries).features)
+    .data(geojson.features)
     .enter()
     .append("path")
     .attr("d", path);
-});
+})
 
-// var path = d3.geoPath().projection(d3.mercator);
-
-// var svg = d3.select("body")
-//   .append("svg")
-//   .attr("width", 500)
-//   .attr("height", 400);
-
-// d3.json("data/world.geo.json").then(function(world) {
+// var svg = d3.select("body").append("svg");
+// var path = d3.geoPath().projection(d3.geoMercator());
+// d3.json("data/world.json").then(function(world) {
+//   console.log(world)
 //   svg.selectAll("path")
-//     .data(world)
+//     .data(topojson.feature(world,world.objects.countries).features)
 //     .enter()
 //     .append("path")
 //     .attr("d", path);
-// });
-
-// d3.json("uk.json", function(error, uk) {
-//   if (error) return console.error(error);
-//   console.log(uk);
 // });
