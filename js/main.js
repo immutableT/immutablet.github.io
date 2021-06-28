111515e2cebfdf98a2b54153661aec18e6b62ed9
immutablet <atcherniakhovski@gmail.com>
2021-06-27 20:06:20 -0500

    World map.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
-// import * as d3 from "d3";
+// import * as d3 from "./d3.min.js";
 
-d3.csv("data/overall.csv").then(function(data) {
-  console.log(data[0]);
+
+// d3.csv("data/overall.csv").then(function(data) {
+//   console.log(data[0]);
+// });
+
+
+var svg = d3.select("body").append("svg").attr("width", 1020).attr("height", 600);
+var path = d3.geoPath().projection(d3.geoMercator());
+d3.json("data/world.json").then(function(world) {
+  console.log(world)
+  svg.selectAll("path")
+    .data(topojson.feature(world,world.objects.countries).features)
+    .enter()
+    .append("path")
+    .attr("d", path);
 });
 
+// var path = d3.geoPath().projection(d3.mercator);
 
+// var svg = d3.select("body")
+//   .append("svg")
+//   .attr("width", 500)
+//   .attr("height", 400);
+
+// d3.json("data/world.geo.json").then(function(world) {
+//   svg.selectAll("path")
+//     .data(world)
+//     .enter()
+//     .append("path")
+//     .attr("d", path);
+// });
+
+// d3.json("uk.json", function(error, uk) {
+//   if (error) return console.error(error);
+//   console.log(uk);
+// });
