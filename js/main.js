72e4c20f9964a1344b8698ec4032335cdb30df23
immutablet <atcherniakhovski@gmail.com>
2021-07-03 18:27:01 -0500

    Data fix for join.

--- js/main.js
+++ js/main.js
@@ -4,11 +4,49 @@ let margin = {top: 0, right: 0, bottom: 0, left: 0};
 let width  = 960 - margin.left - margin.right;
 let height = 400 - margin.top - margin.bottom;
 let projection = d3.geoMercator().scale(130).translate( [width / 2, height / 1.5]);
+const color = d3.scaleQuantize()
+  .range([
+    "rgb(24,64,142)",
+    "rgb(73,93,154)",
+    "rgb(184,186,194)",
+    "rgb(139,96,134)",
+    "rgb(172,15,83)"]
+  );
+
+
+d3.csv("data/overall.csv").then(function(data) {
+  let min = d3.min(data, function(d) { return d["2021"]; });
+  let max = d3.max(data, function(d) { return d["2021"]; });
+
+  color.domain([min, max]);
+  console.log(min, max);
+
+  d3.json("data/world.geo.json").then(function(geojson) {
+    for (let i = 0; i < data.length; i++) {
+      let dataCountry = data[i].Country;
+      let score = parseFloat(data[i]["2021"]);
+      // console.log(`${dataCountry} ${score}`)
+
+      let found = false;
+      for (let j = 0; j < geojson.features.length; j++) {
+        if (dataCountry === geojson.features[j].properties.name) {
+          geojson.features[j].properties["2021"] = score;
+          found = true;
+          // console.log(`Matched ${dataCountry}`)
+          break;
+        }
+      }
+
+      if (!found) {
+        console.log(`Did not match on ${dataCountry}`)
+      }
+    }
+
+  });
+});
 
 let svg = d3.select("body").append("svg");
 let path = d3.geoPath().projection(projection);
-// let url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
-
 d3.json("data/world.geo.json").then(function(geojson) {
   // svg.append("path").attr("d", path(geojson));
   svg.selectAll("path")
@@ -16,9 +54,7 @@ d3.json("data/world.geo.json").then(function(geojson) {
     .enter()
     .append("path")
     .attr("d", path)
-    .style("fill", "steelblue");;
+    .style("fill", "steelblue");
 })
 
-d3.csv("data/overall.csv").then(function(data) {
-  console.log(data[0]);
-});
+
