d68f98cc669ace4cea0df6e19eb6eb94fa280fcd
immutablet <atcherniakhovski@gmail.com>
2021-07-26 20:47:47 -0500

    Added Linear scales.

--- js/gpi-wbi.js
+++ js/gpi-wbi.js
@@ -1,32 +1,49 @@
+export function CreateGPI2WBIScatterPlot(year) {
+  var w = 800;
+  var h = 400;
 
+  let gpiKey = `gpi-${year}`;
+  let wbiKey = `wbi-${year}`;
 
-export function CreateGPI2WBIScatterPlot() {
-  var w = 500;
-  var h = 100;
+  d3.csv('./data/gpi-wbi.csv').then(function(data) {
 
-  var dataset = [
-    [5, 20], [480, 90], [250, 50], [100, 33], [330, 95],
-    [410, 12], [475, 44], [25, 67], [85, 21], [220, 88]
-  ];
+    let xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, function (d) {
+        return d[gpiKey];
+      })])
+      .range([0, w]);
 
-  let svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    let yScale = d3.scaleLinear()
+      .domain([0, d3.max(data, function (d) {
+        return d[wbiKey];
+      })])
+      .range([0, h]);
 
-  svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function(d) {
-      return d[0];
-    })
-    .attr("cy", function(d) {
-      return d[1];
-    })
-    .attr("r", function(d) {
-      return 5;
-    });
+    let svg = d3.select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        return xScale(parseFloat(d[gpiKey]));
+      })
+      .attr("cy", function (d) {
+        let cy = parseFloat(d[wbiKey]);
+        return yScale(cy);
+        // console.log(`cy: ${cy}`)
+        // if (isNaN(cy)) {
+        //   return 0;
+        // }
+        // return cy;
+      })
+      .attr("r", function (d) {
+        return 5;
+      });
+  });
 }
 
 
