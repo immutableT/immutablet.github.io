def1c25ca356da06b750c8e3f706f8c38578c374
immutablet <atcherniakhovski@gmail.com>
2021-07-26 22:19:50 -0500

    Filter-out NaN.

--- js/gpi-wbi.js
+++ js/gpi-wbi.js
@@ -1,6 +1,8 @@
+const margin = {top: 20, right: 30, bottom: 40, left: 30};
+
 export function CreateGPI2WBIScatterPlot(year) {
   var w = 800;
-  var h = 400;
+  var h = 600;
 
   let gpiKey = `gpi-${year}`;
   let wbiKey = `wbi-${year}`;
@@ -9,30 +11,34 @@ export function CreateGPI2WBIScatterPlot(year) {
 
     let xScale = d3.scaleLinear()
       .domain([0, d3.max(data, function (d) {
-        return d[gpiKey];
+        return parseFloat(d[gpiKey]);
       })])
       .range([0, w]);
 
     let yScale = d3.scaleLinear()
       .domain([0, d3.max(data, function (d) {
-        return d[wbiKey];
+        return parseInt(d[wbiKey]);
       })])
       .range([0, h]);
 
-    let svg = d3.select("body")
-      .append("svg")
+    console.log(`${yScale.domain()}`)
+
+    let svg = d3.select("#scatter-plot")
       .attr("width", w)
-      .attr("height", h);
+      .attr("height", h)
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     svg.selectAll("circle")
       .data(data)
       .enter()
+      .filter(function(d) { return !isNaN(parseInt(d[wbiKey]))})
       .append("circle")
       .attr("cx", function (d) {
         return xScale(parseFloat(d[gpiKey]));
       })
       .attr("cy", function (d) {
-        let cy = parseFloat(d[wbiKey]);
+        let cy = parseInt(d[wbiKey]);
+        console.log(`cy: ${yScale(cy)}`)
         return yScale(cy);
         // console.log(`cy: ${cy}`)
         // if (isNaN(cy)) {
