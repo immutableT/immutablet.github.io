4d90786b3f50f5e03758d5f2a8f2a5044baa70cf
atcherni <alex.tcherniakhovski@citadel.com>
2021-07-30 16:57:32 -0500

    Index page is ready.

--- js/color-legend.js
+++ js/color-legend.js
@@ -1,11 +1,11 @@
 export function CreateLegend(containerID, showDataNotAvail) {
   let colorLegend = [
     {color: "495D9AFF", text: "Data not available" },
-    {color: "rgb(14,63,153)", text: `1.5 "&#8593;" More Peaceful"`},
+    {color: "rgb(14,63,153)", text: `1.5 &#8593; More Peaceful"`},
     {color: "rgb(73,93,154)", text:"2.0"},
     {color: "rgb(155,163,193)", text:"2.5"},
     {color: "rgb(206,136,125)", text: "3.0"},
-    {color: "rgb(196,58,31)", text:`3.5" "&#8595" Less Peaceful`}
+    {color: "rgb(196,58,31)", text:`3.5" &#8595 Less Peaceful`}
   ]
   if (!showDataNotAvail) {
     colorLegend.shift();
@@ -16,7 +16,7 @@ export function CreateLegend(containerID, showDataNotAvail) {
   let r = 6;
   let fontSize = "17px"
 
-  let y = 130;CE887DFFCE887DFF
+  let y = 130;
   for (let i = 0; i < colorLegend.length; i++) {
     svg.append("circle")
       .attr("cx", x)
